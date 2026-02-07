b6a557744b00e9883b49e9b2b327f04da26dfd2e
Prateek Rai <prateekrai903@gmail.com>
2026-02-07 15:51:44 +0530

    github scraper springboot implementation

--- server/src/main/java/com/skillloop/server/service/GithubService.java
+++ server/src/main/java/com/skillloop/server/service/GithubService.java
@@ -8,12 +8,10 @@ import com.skillloop.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 @Service
 public class GithubService {
 
